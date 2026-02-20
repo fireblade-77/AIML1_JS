8e5e8928e1cb6ae91a72d3a4b6e64293f96ed3b1
Amaan <amaanstab10@gmail.com>
2026-02-20 10:32:12 +0530

    First Class

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Hello World");
+// console.log("Hello World");
+// document.write("Welcome");
+
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+
+num = false;
+console.log(num);
+console.log(typeof(num));
+
+num = "Sanjeev soni";
+console.log(num);
+console.log(typeof(num));
+
+num = null;
+console.log(num);
+console.log(typeof(num));
+
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+
+
+const sym1 = Symbol(4)
+console.log(sym1);
+
+const sym2 = Symbol(4);
+console.log(sym2);
+
+if(sym1 === sym2){
+    console.log("true")
+
+}
+
+else{
+    console.log("false")
+}
+
+
+var a = 100; var b = 200; var c = 300; var linebreak = "<br>";
+
+document.write("a + b + c=");
+result= a+b+c;
